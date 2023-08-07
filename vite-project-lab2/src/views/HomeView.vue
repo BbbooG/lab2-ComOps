9cce4b7fc57ab3310c057c9d82627aafd0ead1eb
Baibua <siriyakorn14869@gmail.com>
2023-08-07 16:18:21 +0700

    4.12

--- vite-project-lab2/src/views/HomeView.vue
+++ vite-project-lab2/src/views/HomeView.vue
@@ -32,6 +32,6 @@ const events = ref<EventItem[]> ([
 
 <template>
   <main>
-    <EventCard></EventCard>
+    <EventCard v-for="event in events" :key="event.id" :event="event"></EventCard>
   </main>
 </template>

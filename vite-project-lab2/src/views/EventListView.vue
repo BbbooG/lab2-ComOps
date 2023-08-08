c8c3362d218badf405212fea9a1f6204b9dd74e7
Baibua <siriyakorn14869@gmail.com>
2023-08-08 21:46:17 +0700

    6-6.8

--- vite-project-lab2/src/views/EventListView.vue
+++ vite-project-lab2/src/views/EventListView.vue
@@ -3,32 +3,15 @@ import EventCard from '../components/EventCard.vue'
 import EventOrganizer from '../components/EventOrganizer.vue'
 import  type { EventItem } from '@/type';
 
-import { ref } from 'vue'
-const events = ref<EventItem[]> ([
-  {
-    id: 5928101,
-    category: 'animal welfare',
-    title: 'Cat Adoption Day',
-    description: 'Find your new feline friend at this event.',
-    location: 'Meow Town',
-    date: 'January 28, 2022',
-    time: '12:00',
-    petsAllowed: true,
-    organizer: 'Kat Laydee'
-  },
 
-  {
-    id: 8419988,
-    category: 'sustainability',
-    title: 'Beach Cleanup',
-    description: 'Help pick up trash along the shore.',
-    location: 'Playa Del Carmen',
-    date: 'July 22, 2022',
-    time: '11:00',
-    petsAllowed: false,
-    organizer: 'Carey Wales'
-  }
-])
+import { ref, Ref } from 'vue'
+import axios from 'axios'
+const events: Ref<Array<EventItem>> = ref([])
+
+axios.get<EventItem[]>('http://localhost:3004/events')
+.then((response) => {
+  events.value = response.data
+})
 </script>
 
 <template>
